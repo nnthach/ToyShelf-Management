5e6e682a326feef3d34bef3c32d84742c9d3e650
nnthach <nguyenngocthach2301@gmail.com>
2026-03-09 15:12:58 +0700

    Edit sth

--- src/app/admin/(product)/products/create/ConfirmPopup.tsx
+++ src/app/admin/(product)/products/create/ConfirmPopup.tsx
@@ -67,7 +67,6 @@ function ConfirmPopup({
                     {previewData?.weight != null
                       ? Number(previewData.weight)
                       : "--"}{" "}
-                    {previewData?.unit}
                   </p>
                 </div>
               </div>
